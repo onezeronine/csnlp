f9c9374ef0724586b34b31b328ca9535617dc7f0
onezeronine <bastian.kenneth.g@gmail.com>
2015-10-05 10:57:54 +0800

    added code analysis

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,20 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
 
-gulp.task('unit-test', function() {
+gulp.task('analysis', function() {
+  return gulp
+    .src('./csnlp.js')
+    .pipe(jshint())
+    .pipe(jscs())
+    .pipe(jscs.reporter())
+    .pipe(jscs.reporter('fail'))
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('spectest', ['analysis'], function() {
   return gulp
     .src('./test/*.js', {read: false})
     .pipe(mocha());
