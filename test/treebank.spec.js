1e34aacd8890ec914b0545a396995137ad25bc3c
onezeronine <bastian.kenneth.g@gmail.com>
2015-10-05 11:48:59 +0800

    fixed test for treebank

--- test/treebank.spec.js
+++ test/treebank.spec.js
@@ -1,15 +1,33 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var csnlp = require('../csnlp');
 
+/*jshint -W030 */
 describe('csnlp', function() {
-  describe('#tokenizeTB()', function() {
-    it('null argument', function() {
+  describe('#treebank()', function() {
+
+    it('Tokenize null', function() {
       var tokens = csnlp.treebank(null);
-      assert.equal(0, tokens.length);
+      expect(tokens).to.exist;
+      expect(tokens).to.be.a('array');
     });
-    it('empty string', function() {
+
+    it('Tokenize empty string', function() {
       var tokens = csnlp.treebank('');
-      assert.equal(0, tokens.length);
+      expect(tokens).to.empty;
+      expect(tokens).to.be.a('array');
+      expect(tokens).to.have.length(0);
+    });
+
+    it('Tokenize complete sentence', function() {
+      var tokens = csnlp.treebank('The quick brown fox jumps over the lazy dog.');
+      var expected = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog', '.'];
+      expect(tokens).to.exist;
+      expect(tokens).to.be.a('array');
+      expect(tokens).to.have.length(expected.length);
+
+      for(var i = 0; i < tokens.length; ++i) {
+        expect(tokens[i]).to.be.equal(expected[i]);
+      }
     });
   });
 });
